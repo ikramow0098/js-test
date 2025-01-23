3f7ac23fb4ae610920b1f6d5f9366f15af15887b
Fotihjon <ikromov@usf.edu>
2025-01-23 13:04:39 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -4,3 +4,10 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+let productName = "Tomato"
+const productPrice = 9123;
+var isAvailable = false;
+console.log(productName, typeof(productName));
+console.log(productPrice, typeof(productPrice));
+console.log(isAvailable, typeof(isAvailable));
