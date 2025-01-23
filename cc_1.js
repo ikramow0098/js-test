28e860c6f2138265d9db54d9e20227b08fa0f432
Fotihjon <ikromov@usf.edu>
2025-01-23 13:02:14 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,6 @@
+let employeeName = "John";
+const employeeID = 123;
+var isActive = true;
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
